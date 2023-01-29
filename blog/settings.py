5f9f7df77e3ba98c8edef1b344b573d30ec7de03
Sash <sasheng04@gmail.com>
2023-01-29 08:47:19 +0000

    Adjust secret key variable

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nwue_odrtj8*x4-u%4648r%a-pbow11=t6n$nfmn5_hltz42h*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
