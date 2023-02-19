e0e591c0fbedb22aa03c84d576798e07f1185387
Sash <sasheng04@gmail.com>
2023-02-19 09:15:30 +0000

    Add import for climb in views.py

--- OFF_THE_SADDLE/urls.py
+++ OFF_THE_SADDLE/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.ClimbList.as_view(), name="climb"),
+    path("", views.ClimbList.as_view(), name="home"),
     ]
