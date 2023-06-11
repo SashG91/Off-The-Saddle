2fe6f3f53cc0504f88986feef7ba9bb00f7f8b04
Sash <sasheng04@gmail.com>
2023-06-11 22:25:32 +0000

    Add views and urls for custom error pages

--- OFF_THE_SADDLE/urls.py
+++ OFF_THE_SADDLE/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.ClimbList.as_view(), name='home'),
     path('<slug:slug>/', views.ClimbDetail.as_view(), name='climb_detail'),
+    path('403', views.Page403.as_view(), name='403'),
+    path('404', views.Page404.as_view(), name='404'),
+    path('500', views.Page500.as_view(), name='500'),
     # path('<slug:slug>/add_time', views.AddClimbTime.as_view(), name='add_time')
 ]
